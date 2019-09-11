e16c0a13549a8938a92fc3a7c9214ce99e2fabb7
dong <1412834290@qq.com>
2019-09-11 19:39:27 +0800

    init commit

---
+++ student-demo-common-impl/src/main/java/com/practice/service/StudentServiceImpl.java
@@ -0,0 +1,13 @@
+package com.practice.service;
+
+/**
+ * @ProjectName: student-demo
+ * @Package: com.practice.service
+ * @ClassName: StudentServiceImpl
+ * @Author: dong
+ * @Description: ${description}
+ * @Date: 2019/9/11 19:17
+ * @Version: 1.0
+ */
+public class StudentServiceImpl {
+}
---
+++ student-demo-common-inf/src/main/java/com/practice/service/StudentService.java
@@ -0,0 +1,16 @@
+package com.practice.service;
+
+import java.util.List;
+
+/**
+ * @ProjectName: student-demo
+ * @Package: com.practice.service
+ * @ClassName: StudentService
+ * @Author: dong
+ * @Description: ${description}
+ * @Date: 2019/9/11 19:18
+ * @Version: 1.0
+ */
+public interface StudentService {
+//    List<Student> queryAllStudents();
+}
